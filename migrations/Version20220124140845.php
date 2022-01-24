2ad5f687ba3cdb4ec44e2b80d655bc32423420a2
BertrandBurel <legionxful@gmail.com>
2022-01-24 15:15:15 +0100

    Modify entities relation Etat - Sortie

---
+++ migrations/Version20220124140845.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220124140845 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE participant ADD pseudo VARCHAR(50) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_D79F6B1186CC499D ON participant (pseudo)');
+        $this->addSql('ALTER TABLE sortie ADD etat_id INT NOT NULL');
+        $this->addSql('ALTER TABLE sortie ADD CONSTRAINT FK_3C3FD3F2D5E86FF FOREIGN KEY (etat_id) REFERENCES etat (id)');
+        $this->addSql('CREATE INDEX IDX_3C3FD3F2D5E86FF ON sortie (etat_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX UNIQ_D79F6B1186CC499D ON participant');
+        $this->addSql('ALTER TABLE participant DROP pseudo');
+        $this->addSql('ALTER TABLE sortie DROP FOREIGN KEY FK_3C3FD3F2D5E86FF');
+        $this->addSql('DROP INDEX IDX_3C3FD3F2D5E86FF ON sortie');
+        $this->addSql('ALTER TABLE sortie DROP etat_id');
+    }
+}
