35ec94337e720a7bf5e7eb5cec7a9c8e08578a1f
christanvt <christanvt83@gmail.com>
2022-01-26 21:47:21 +0100

    Ajout de plusieurs fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,20 +2,33 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Campus;
+use Faker;
 use App\Entity\Etat;
 use App\Entity\Lieu;
+use App\Entity\Participant;
 use App\Entity\Ville;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 
 class AppFixtures extends Fixture
 {
+    private $encoder;
+    public function __construct(UserPasswordHasherInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
     public function load(ObjectManager $manager): void
     {
         $this->manager = $manager;
         $this->loadEtat();
         $this->loadVilles();
         $this->loadLieux();
+        $this->loadCampus();
+        $this->loadAdmin();
+        $this->loadParticipants(10);
     }
     public function loadEtat(): void
     {
@@ -75,6 +88,77 @@ class AppFixtures extends Fixture
             ->setLongitude($longitude)
             ->setLatitude($latitude);
         $this->manager->persist($lieu);
+        $this->manager->flush();
+    }
+    public function loadCampus(): void
+    {
+        $campusNomsArray = [
+            'SAINT-HERBLAIN',
+            'CHARTRES DE BRETAGNE',
+            'LA ROCHE SUR YON',
+        ];
+        for ($i = 0; $i < count($campusNomsArray); $i++) {
+            $nom = $campusNomsArray[$i];
+            $campus = new Campus();
+            $campus->setNom($nom);
+            $this->manager->persist($campus);
+        }
+        $this->manager->flush();
+    }
+    public function loadAdmin(): void
+    {
+        $participant = new Participant;
+        $nom = "ADMIN";
+        $prenom = "ADMIN";
+        $pseudo = "ADMIN";
+        $administrateur = 1;
+        $actif = 1;
+        $telephone = "0123456789";
+        $email = "admin@admin.fr";
+        $nomCampus = "SAINT-HERBLAIN";
+        $campus = $this->manager->getRepository(Campus::class)->findOneBy(['nom' => $nomCampus]);
+        $password = $this->encoder->hashPassword($participant, 'admin');
+        $participant
+            ->setNom($nom)
+            ->setPrenom($prenom)
+            ->setPseudo($pseudo)
+            ->setAdministrateur($administrateur)
+            ->setActif($actif)
+            ->setTelephone($telephone)
+            ->setEmail($email)
+            ->setCampus($campus)
+            ->setMotpasse($password);
+        $this->manager->persist($participant);
+        $this->manager->flush();
+    }
+    public function loadParticipants(int $count): void
+    {
+        $faker = Faker\Factory::create('fr_FR');
+        for ($i = 0; $i < $count; $i++) {
+
+            $participant = new Participant;
+            $nom = $faker->lastName();
+            $prenom = $faker->firstname();
+            $pseudo = $prenom . '.' . $nom;
+            $administrateur = 0;
+            $actif = 1;
+            $telephone = $faker->mobileNumber();
+            $mailDomain = $faker->freeEmailDomain();
+            $email = $prenom . '.' . $nom . '@' . $mailDomain;
+            $campus = $this->manager->getRepository(Campus::class)->findAll()[random_int(0, 2)];
+            $password = $this->encoder->hashPassword($participant, $pseudo);
+            $participant
+                ->setNom($nom)
+                ->setPrenom($prenom)
+                ->setPseudo($pseudo)
+                ->setAdministrateur($administrateur)
+                ->setActif($actif)
+                ->setTelephone($telephone)
+                ->setEmail($email)
+                ->setCampus($campus)
+                ->setMotpasse($password);
+            $this->manager->persist($participant);
+        }
         $this->manager->flush();
     }
 }
